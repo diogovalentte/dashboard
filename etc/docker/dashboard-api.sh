93f0f9bc7747cbb4ed8bcd74e764176d1c2b0bee
Diogo Valentim <diogovalentte10@gmail.com>
2023-09-27 15:39:11 -0300

    Fix docker-compose.yml

--- etc/docker/dashboard-api.sh
+++ etc/docker/dashboard-api.sh
@@ -7,7 +7,6 @@ curl -L -o /tmp/firefox-117.0.1.tar.bz2 "https://download.mozilla.org/?product=f
 tar -xvf /tmp/firefox-117.0.1.tar.bz2 -C ./etc/
 
 curl -L -o /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz
-mkdir -p /opt/geckodriver/
 mkdir -p etc/geckodriver
 tar -xvf /tmp/geckodriver.tar.gz -C ./etc/geckodriver/
 
