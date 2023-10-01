05bf156f5b08a140b61c6215764cb11534cf38ea
Diogo Valentim <diogovalentte10@gmail.com>
2023-10-01 15:05:44 -0300

    Update docker services/containers

--- etc/docker/dashboard-api.sh
+++ etc/docker/dashboard-api.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-apt update
-apt install -y bzip2 curl
-
-# Install golang
-wget -O /tmp/go1.21.1.linux-amd64.tar.gz https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
-tar -C /tmp/ -xzf /tmp/go1.21.1.linux-amd64.tar.gz
-export PATH=$PATH:/tmp/go/bin
-
-# Install firefox v117.0.1
-wget -O /tmp/firefox-117.0.1.tar.bz2 "https://download.mozilla.org/?product=firefox-117.0.1&os=linux64&lang=en-US"
-sudo tar -C /opt -xvf /tmp/firefox-117.0.1.tar.bz2
-
 # Start API
+export PATH=$PATH:/tmp/go/bin
 go build -o dashboard-api main.go
 ./dashboard-api
